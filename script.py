83bf937066fcd9027bcec28df69ea68a5f3b196f
tcvanthiel <t.c.vanthiel@tudelft.nl>
2018-11-07 13:30:53 +0100

    Created script.py which prints the results of the trigonometry functions defined in test.py

---
+++ script.py
@@ -0,0 +1,8 @@
+import test
+
+radius = 2
+
+circumference = test.calc_circumference(radius)
+area = test.calc_area(radius)
+
+print('For a circle of radius '+str(radius)+', the circumference is '+str(circumference)+' and the area is '+str(area)+'.')
